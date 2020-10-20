663be17dbb7aa471f4e276931c640adb96f5d288
Volodymyr Vyshnenko <vyshnenko2904@gmail.com>
2020-10-20 22:36:45 +0300

    Добавьте файлы проекта.

---
+++ 8/Invoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _8
+{
+    internal class Invoice
+    {
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        private int account;
+        private string customer;
+        private string provider;
+        private int price;
+
+        public Invoice(int account, string customer, string provider, int price)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+            this.price = price;
+            Article = "";
+            Quantity = 0;
+        }
+        private double PriceWithVAT()
+        {
+            return price * Quantity * 1.2;
+        }
+        private double PriceWithoutVAT()
+        {
+            return (double)price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Account №" + account + "\nCustomer: " + customer + "\nProvider: " + provider + "\nProduct info:\narticle - " + Article
+                + ", quantity - " + Quantity + ", price per unit - " + price + "\nTotal price without VAT: " + PriceWithoutVAT().ToString() + ", with VAT: " + PriceWithVAT().ToString();
+        }
+    }
+}
---
+++ 8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice inv = new Invoice(123456789, "Petr Petrov", "Bober Bobrov", 5678);
+            inv.Article = "oak stick";
+            inv.Quantity = 789;
+            Console.WriteLine(inv.ToString());
+            Console.ReadKey();
+        }
+    }
+}
